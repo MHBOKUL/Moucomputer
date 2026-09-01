c2c9f61861376390fa1239b61f26c86540477965
Mehedi <mehedibok5@gmail.com>
2026-09-02 02:43:49 +0600

    Fix divisions table migrations

--- database/migrations/2026_08_29_085533_create_divisions_table.php
+++ database/migrations/2026_08_29_085533_create_divisions_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('divisions', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('name_bn')->nullable();
             $table->timestamps();
         });
     }
